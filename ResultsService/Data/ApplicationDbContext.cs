d4efb097b1e22c0de4cac75b57df5781147536e5
Hani Alija <ha30049@seeu.edu.mk>
2024-06-02 13:45:41 +0200

    Results

---
+++ ResultsService/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ResultsService.Models;
+
+namespace ResultsService.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Result> Results { get; set; }
+    }
+}
